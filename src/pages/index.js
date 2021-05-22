c82b9ff27e942a12f6ea53e94d4743d55352433c
jwilcox501 <speaktojohn@btinternet.com>
2021-05-22 21:51:24 +0100

    second Change

--- src/pages/index.js
+++ src/pages/index.js
@@ -120,7 +120,7 @@ const links = [
     url: "https://www.gatsbyjs.com/cloud",
     badge: true,
     description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free! Sha yea!",
     color: "#663399",
   },
 ]
